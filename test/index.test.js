9155bfa8662eb3185d049bb23e698fee2d3cef07
kzhang <zhkmail@foxmail.com>
2019-03-01 10:17:23 +0800

    fix set

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ const Redis = require('../lib')
 const redis = new Redis();
 
 test('two arguments set and get through callback', done => {
+  expect.assertions(1)
   redis.set('foo', 'bar');
   redis.get('foo', function (err, result) {
     expect(result).toBe('bar');
@@ -11,6 +12,7 @@ test('two arguments set and get through callback', done => {
 })
 
 test('promise get', ()=>{
+  expect.assertions(1)
   redis.set('foo', 'bar1');
   return redis.get('foo').then(result => {
     expect(result).toBe('bar1')
@@ -18,22 +20,26 @@ test('promise get', ()=>{
 })
  
 test('three arguments set and get', async() => {
+  expect.assertions(3)
   expect(await redis.set('a', '1', 1000)).toBe('OK');
   expect(await redis.get('a')).toBe('1')
   expect(await redis.pttl('a')).toBeLessThan(1000)
 })
 
 test('duplicate set', async()=>{
+  expect.assertions(1)
   expect(await redis.setnx('foo', 'bar')).toBe(0);
 })
 
 test('All arguments are passed directly to the redis server', async()=>{
+  expect.assertions(3)
   expect(await redis.set('key', 100, 'EX', 200)).toBe('OK')
-  expect(await redis.get('key')).toBe(100)
+  expect(await redis.get('key')).toBe('100')
   expect(await redis.ttl('key')).toBe(200)
 })
 
 test('other redis orders', ()=>{
+  expect.assertions(1)
   redis.sadd('set', 1, 3, 5, 7);
   redis.sadd('set', [2, 4, 5, 7]);
   return redis.scard('set').then(value => {
